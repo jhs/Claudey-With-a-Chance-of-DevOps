aa52cc832fd0319bdf73d26b1432b87c392eb7f6
Jason Smith (FS) <jason.h.smith@gmail.com>
2025-07-30 17:42:36 -0500

    Working link to noVNC

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
+  const url = `http://localhost:6901/?password=password`
+
   return (
     <div className="w-full min-h-screen">
       <iframe
-        src="http://localhost:6080/vnc.html"
+        src={url}
         className="w-full h-full border-0"
         title="VNC Interface"
         style={{height: '920px', width: '100%'}}
